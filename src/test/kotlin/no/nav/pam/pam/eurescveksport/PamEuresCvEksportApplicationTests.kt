077fb300bdd120b1fe2979c4bd18eb5b38e4ade1
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2020-03-05 09:36:29 +0100

    Initial Commit

---
+++ src/test/kotlin/no/nav/pam/pam/eurescveksport/PamEuresCvEksportApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pam.pam.eurescveksport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PamEuresCvEksportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
