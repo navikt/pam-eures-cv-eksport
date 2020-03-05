b3116b69a106eff43ee082a4440f960fddd16ea1
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2020-03-05 11:34:36 +0100

    Added basic kafka-listener.
    Configured to consume the dev-topic created in pam-cv-api, see https://github.com/navikt/pam-cv-api

---
+++ src/main/kotlin/no/nav/pam/pam/eurescveksport/kafka/CvConsumer.kt
@@ -0,0 +1,16 @@
+package no.nav.pam.pam.eurescveksport.kafka
+
+import no.nav.arbeid.cv.avro.Melding
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
+class CvConsumer {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    @KafkaListener(topics = ["\${kafka.cvtopic}"])
+    fun processMessage(record: ConsumerRecord<String, Melding>) {
+        logger.info("got message: {}", record)
+    }
+}
