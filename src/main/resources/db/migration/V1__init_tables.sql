27beeffab8ca8a35c02969361cc8aa243136d18a
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-06-11 09:49:31 +0200

    Fjerner gammel kode, og begynner på nytt

--- src/main/resources/db/migration/V1__init_tables.sql
+++
@@ -1,7 +0,0 @@
-create table cv
-(
-    id             varchar(128) not null primary key,
-    opprettet_ts   timestamp    not null,
-    sist_endret_ts timestamp    not null,
-    json_cv        text         not null
-);
