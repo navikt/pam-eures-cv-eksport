bfd9a0fec18acaae9ecd88e9e0cc1759e3665373
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-07-06 13:11:13 +0200

    Få en Kafka consumer opp og stå, uten å bruke Micronaut Kafka. Foreløpig kun lokal maskin.

---
+++ src/main/kotlin/no/nav/cv/eures/cv/CvConsumer.kt
@@ -0,0 +1,49 @@
+package no.nav.cv.eures.cv
+
+import io.micronaut.context.annotation.Value
+import io.micronaut.scheduling.annotation.Scheduled
+import org.slf4j.LoggerFactory
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import java.time.Duration
+import java.util.*
+import javax.inject.Singleton
+
+@Singleton
+class CvConsumer(
+        @Value("\${kafka.brokers.cv_endret}") private val brokers: String,
+        @Value("\${kafka.topics.consumers.cv_endret}") private val topic: String,
+        @Value("\${kafka.topics.consumers.group_id}") private val groupId: String
+) {
+
+    private val consumer = createConsumer()
+
+    companion object {
+        val log = LoggerFactory.getLogger(CvConsumer::class.java)
+    }
+
+    @Scheduled(fixedDelay = "15s")
+    fun process() {
+        log.info("Process() starter")
+        
+        val endredeCVer = consumer.poll(Duration.ofSeconds(1))
+
+        log.info("Fikk ${endredeCVer.count()} CVer")
+
+        for(cv in endredeCVer) {
+            log.info("Fikk CV $cv")
+        }
+    }
+
+    private fun createConsumer() : Consumer<String, String> {
+        val props = Properties()
+        props["bootstrap.servers"] = brokers
+        props["group.id"] = groupId
+        props["key.deserializer"] = StringDeserializer::class.java
+        props["value.deserializer"] = StringDeserializer::class.java
+        val consumer = KafkaConsumer<String, String>(props)
+        consumer.subscribe(listOf(topic))
+        return consumer
+    }
+}
