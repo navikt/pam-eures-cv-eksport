bd6fa04bda49ad42eed68d4461ee09d977ed70d5
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-07-07 13:38:18 +0200

    Tester også om cv oppdateres og at ukjent aktør id gir null.

--- src/test/kotlin/no/nav/cv/CvRepositoryTest.kt
+++ src/test/kotlin/no/nav/cv/CvRepositoryTest.kt
@@ -3,10 +3,13 @@ package no.nav.cv
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
+import assertk.assertions.isNull
 import io.micronaut.test.annotation.MicronautTest
 import no.nav.cv.eures.cv.CvRepository
 import no.nav.cv.eures.cv.RawCV
+import org.hibernate.exception.ConstraintViolationException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.ZonedDateTime
 import javax.inject.Inject
 
@@ -19,43 +22,74 @@ class CvRepositoryTest {
     private val now = ZonedDateTime.now()
     private val yesterday = ZonedDateTime.now().minusDays(1)
 
+    private val aktorId1 = "123"
+    private val aktorId2 = "321"
+    private val aktorIdUkjent = "ukjent"
+
+    private val rawAvro1 = "raw avro string 1"
+    private val rawAvro2 = "raw avro string 2"
+
+
     @Test
     fun `finn cv knyttet til aktorid`() {
-        val cv = RawCV.create("123", now, "raw avro string")
+        val cv = RawCV.create(aktorId1, now, rawAvro1)
 
         cvRepository.lagreCv(cv)
 
-        val hentet = cvRepository.hentCv("123")
+        val hentet = cvRepository.hentCv(aktorId1)
 
         assertThat(hentet).isNotNull()
-        assertThat(hentet?.aktorId).isEqualTo("123")
+        assertThat(hentet?.aktorId).isEqualTo(aktorId1)
         assertThat(hentet?.sistEndret).isEqualTo(now)
-        assertThat(hentet?.rawAvro).isEqualTo("raw avro string")
+        assertThat(hentet?.rawAvro).isEqualTo(rawAvro1)
     }
 
     @Test
     fun `finn en av flere cv`() {
-        val cv1 = RawCV.create("123", now, "raw avro string 1")
-        val cv2 = RawCV.create("321", yesterday, "raw avro string 2")
+        val cv1 = RawCV.create(aktorId1, now, rawAvro1)
+        val cv2 = RawCV.create(aktorId2, yesterday, rawAvro2)
 
         cvRepository.lagreCv(cv1)
         cvRepository.lagreCv(cv2)
 
-        val hentet1 = cvRepository.hentCv("123")
+        val hentet1 = cvRepository.hentCv(aktorId1)
 
         assertThat(hentet1).isNotNull()
-        assertThat(hentet1?.aktorId).isEqualTo("123")
+        assertThat(hentet1?.aktorId).isEqualTo(aktorId1)
         assertThat(hentet1?.sistEndret).isEqualTo(now)
-        assertThat(hentet1?.rawAvro).isEqualTo("raw avro string 1")
+        assertThat(hentet1?.rawAvro).isEqualTo(rawAvro1)
 
-        val hentet2 = cvRepository.hentCv("321")
+        val hentet2 = cvRepository.hentCv(aktorId2)
 
         assertThat(hentet2).isNotNull()
-        assertThat(hentet2?.aktorId).isEqualTo("321")
+        assertThat(hentet2?.aktorId).isEqualTo(aktorId2)
         assertThat(hentet2?.sistEndret).isEqualTo(yesterday)
-        assertThat(hentet2?.rawAvro).isEqualTo("raw avro string 2")
+        assertThat(hentet2?.rawAvro).isEqualTo(rawAvro2)
     }
 
+    @Test
+    fun `cv blir oppdatert`() {
+        val cv1 = RawCV.create(aktorId1, yesterday, rawAvro1)
 
+        cvRepository.lagreCv(cv1)
 
+        val cv2 = cvRepository.hentCv(aktorId1)
+                ?.update(aktorId1, now, rawAvro2)
+
+        assertThat(cv2).isNotNull()
+        cvRepository.lagreCv(cv2!!)
+
+        val hentet2 = cvRepository.hentCv(aktorId1)
+
+        assertThat(hentet2?.aktorId).isEqualTo(aktorId1)
+        assertThat(hentet2?.sistEndret).isEqualTo(now)
+        assertThat(hentet2?.rawAvro).isEqualTo(rawAvro2)
+    }
+
+    @Test
+    fun `ukjent cv gir null`() {
+        val ukjentCv = cvRepository.hentCv(aktorIdUkjent)
+
+        assertThat(ukjentCv).isNull()
+    }
 }
