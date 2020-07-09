35bf036fbede16f86cd6f235f1b67b44a9614aa3
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-07-09 13:20:17 +0200

    Legger til DataSourceFactory.kt for å fasilitere vault/database integrasjonen

---
+++ src/main/kotlin/no/nav/cv/eures/infrastructure/database/DataSourceFactory.kt
@@ -0,0 +1,51 @@
+package no.nav.cv.eures.infrastructure.database
+
+import com.zaxxer.hikari.HikariDataSource
+import io.micronaut.configuration.jdbc.hikari.DatasourceConfiguration
+import io.micronaut.configuration.jdbc.hikari.DatasourceFactory
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.*
+import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil
+import org.slf4j.LoggerFactory
+import javax.annotation.PreDestroy
+
+
+@Factory
+@Requires(notEnv = [ "test" ])
+@Replaces(factory = DatasourceFactory::class)
+class VaultDatasourceFactory(
+        @Value("\${db.vault.path}") private val vaultPath: String,
+        @Value("\${db.vault.username}") private val username: String,
+        private val applicationContext: ApplicationContext
+) : AutoCloseable {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(DatasourceFactory::class.java)
+    }
+
+    private val dataSources: MutableList<HikariDataSource> = mutableListOf()
+
+
+    @Context
+    @EachBean(DatasourceConfiguration::class)
+    fun vaultDataSource(datasourceConfiguration: DatasourceConfiguration ): HikariDataSource {
+        val ds = HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(datasourceConfiguration, vaultPath, username)
+        dataSources.add(ds)
+        return ds
+    }
+
+    @Override
+    @PreDestroy
+    override fun close() {
+        for (dataSource in dataSources) {
+            try {
+                dataSource.close();
+            } catch (e: Exception) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn("Error closing data source [" + dataSource + "]: " + e.message, e);
+                }
+            }
+        }
+    }
+
+}
