8a2ee955ef040b1b8fd306ea260c94897916781e
marthehagen <marthe.hagen@nav.no>
2020-08-25 14:04:57 +0200

    Laget samtykke-db, -repository og -controller

--- src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeController.kt
+++ src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeController.kt
@@ -1,7 +1,10 @@
 package no.nav.cv.eures.samtykke
 
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.*
+import io.micronaut.http.exceptions.HttpException
+import java.lang.Exception
+import java.net.http.HttpResponse
 
 @Controller("samtykke")
 class SamtykkeController(
@@ -9,11 +12,15 @@ class SamtykkeController(
 ) {
 
     @Get("/{aktoerId}", produces = [ "application/json" ])
-    fun hentSamtykke(aktoerId: String) : Samtykke {
-        return samtykkeRepository.hentSamtykke(aktoerId)
-    }
+    fun hentSamtykke(aktoerId: String) : Samtykke
+            = samtykkeRepository.hentSamtykke(aktoerId)
+            ?: throw Exception("not found")
 
+    @Post("/{aktoerId}", produces = [ "application/json" ])
+    fun oppdaterSamtykke(aktoerId: String, @Body samtykke: Samtykke)
+            = samtykkeRepository.oppdaterSamtykke(samtykke)
 
-
-
+    @Delete("/{aktoerId}", produces = [ "application/json" ])
+    fun slettSamtykke(aktoerId: String)
+            = samtykkeRepository.slettSamtykke(aktoerId)
 }
--- src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeRepository.kt
+++ src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeRepository.kt
@@ -1,8 +1,74 @@
 package no.nav.cv.eures.samtykke
 
+import io.micronaut.spring.tx.annotation.Transactional
+import no.nav.cv.eures.cv.RawCV
+import java.time.ZonedDateTime
+import java.util.*
+import javax.inject.Singleton
+import javax.persistence.*
+
 interface SamtykkeRepository {
 
-    fun hentSamtykke(aktoerId: String) : Samtykke
+    fun hentSamtykke(aktoerId: String) : Samtykke?
     fun slettSamtykke(aktoerId: String)
     fun oppdaterSamtykke(samtykke: Samtykke)
+}
+
+@Singleton
+private open class JpaSamtykkeRepository(
+        @PersistenceContext private val entityManager: EntityManager
+) : SamtykkeRepository {
+    private val serieMedWhitespace = Regex("(\\s+)")
+
+    private val hentSamtykke =
+            """
+                SELECT * FROM SAMTYKKE
+                WHERE AKTOER_ID = :aktoerId
+            """.replace(serieMedWhitespace, " ")
+
+    @Transactional(readOnly = true)
+    override fun hentSamtykke(aktoerId: String): Samtykke? {
+        return entityManager.createNativeQuery(hentSamtykke, Samtykke::class.java)
+                .setParameter("aktoerId", aktoerId)
+                .resultList
+                .map { it as SamtykkeEntity }
+                .map { it.toSamtykke() }
+                .firstOrNull()
+    }
+
+    override fun slettSamtykke(aktoerId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun oppdaterSamtykke(samtykke: Samtykke) {
+        TODO("Not yet implemented")
+    }
+}
+
+@Entity
+@Table(name = "SAMTYKKE")
+class SamtykkeEntity() {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(generator = "SAMTYKKE_SEQ")
+    private var id: Long? = null
+
+    @Column(name = "AKTOER_ID", nullable = false, unique = true)
+    lateinit var aktoerId: String
+
+    @Column(name = "SIST_ENDRET", nullable = false)
+    lateinit var sistEndret: ZonedDateTime
+
+    @Column(name = "PERSONALIA", nullable = false)
+    lateinit var personalia: Boolean
+
+    @Column(name = "UTDANNING", nullable = false)
+    lateinit var utdanning: Boolean
+
+
+    fun toSamtykke() = Samtykke(
+            aktoerId = aktoerId,
+            personalia = personalia,
+            utdanning = utdanning
+    )
 }
