787f78377ddcfe7cbd6245f4e135b18c218da58c
Magnus S Espeland <magnus.s.espeland@nav.no>
2020-10-20 13:57:29 +0200

    WIP Tester Micronaut Kafka Consumer

--- src/main/kotlin/no/nav/cv/eures/cv/CvConsumerAdmin.kt
+++ src/main/kotlin/no/nav/cv/eures/cv/CvConsumerAdmin.kt
@@ -1,16 +1,16 @@
-package no.nav.cv.eures.cv
-
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-
-@Controller("internal/cvConsumer")
-class CvConsumerAdmin(
-        private val cvConsumer: CvConsumer
-) {
-
-    @Get("seekToBeginning", produces = ["text/plain"])
-    fun seekToBeginning() : String {
-        cvConsumer.seekToBeginning()
-        return "OK"
-    }
-}
+//package no.nav.cv.eures.cv
+//
+//import io.micronaut.http.annotation.Controller
+//import io.micronaut.http.annotation.Get
+//
+//@Controller("internal/cvConsumer")
+//class CvConsumerAdmin(
+//        private val cvConsumer: CvConsumer
+//) {
+//
+//    @Get("seekToBeginning", produces = ["text/plain"])
+//    fun seekToBeginning() : String {
+//        cvConsumer.seekToBeginning()
+//        return "OK"
+//    }
+//}
