01e0b51b93c44437bae71e23ca1707e006253386
Einar Bjerve <einar.bjerve@nav.no>
2020-11-23 10:58:28 +0100

    Fixed startup

--- src/main/kotlin/no/nav/cv/eures/Application.kt
+++ src/main/kotlin/no/nav/cv/eures/Application.kt
@@ -1,6 +1,7 @@
 package no.nav.cv.eures
 
 import org.springframework.boot.Banner
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -11,5 +12,6 @@ class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args) {
         setBannerMode(Banner.Mode.OFF)
+        webApplicationType = WebApplicationType.NONE
     }
 }
