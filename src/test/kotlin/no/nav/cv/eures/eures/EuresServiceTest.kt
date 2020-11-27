c113238e01dc0ec48461cb26ae779131b20287bd
Einar Bjerve <einar.bjerve@nav.no>
2020-11-27 14:02:13 +0100

    Secured enduser endpoints with token-support

--- src/test/kotlin/no/nav/cv/eures/eures/EuresServiceTest.kt
+++ src/test/kotlin/no/nav/cv/eures/eures/EuresServiceTest.kt
@@ -4,6 +4,7 @@ import no.nav.cv.eures.cv.CvXml
 import no.nav.cv.eures.cv.CvXmlRepository
 import no.nav.cv.eures.eures.dto.GetDetails.CandidateDetail.Status.ACTIVE
 import no.nav.cv.eures.eures.dto.GetDetails.CandidateDetail.Status.CLOSED
+import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
@@ -15,6 +16,7 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import java.time.ZonedDateTime
@@ -23,6 +25,7 @@ import java.time.ZonedDateTime
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Import(TokenGeneratorConfiguration::class)
 class EuresServiceTest {
 
     @Autowired
