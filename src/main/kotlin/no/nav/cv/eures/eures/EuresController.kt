7a8311fa6ab73ee69967c2963006535b99f9661a
Einar Bjerve <einar.bjerve@nav.no>
2020-11-30 09:08:36 +0100

    Skrevet om til HandlerInterceptor som er mer springy måte å gjøre denne type filter på. Da får vi heller ikke sårbare for at endringer i strings fører til at vi ikke lenger har sikret endepunktene våre, siden det er controllerene vi sikrer i handleren stedet for url'en. La også til unittester som testet retur av 401

--- src/main/kotlin/no/nav/cv/eures/eures/EuresController.kt
+++ src/main/kotlin/no/nav/cv/eures/eures/EuresController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("\${eures.uri-part}/\${eures.uri-suffix}")
+@RequestMapping("input/api/cv/v1.0")
 class EuresController(
         private val euresService: EuresService
 ) {
--- src/main/kotlin/no/nav/cv/eures/eures/EuresSecurity.kt
+++ src/main/kotlin/no/nav/cv/eures/eures/EuresSecurity.kt
@@ -6,32 +6,59 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.method.HandlerMethod
+import org.springframework.web.servlet.HandlerAdapter
+import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@Component
-class EuresSecurityFilter(
-        @Value("\${eures.token}") private val ourToken: String,
-        @Value("\${eures.uri-part}") private val uriPart: String,
-) : OncePerRequestFilter() {
-    private val log: Logger = LoggerFactory.getLogger(EuresSecurityFilter::class.java)
+//@Component
+//class EuresSecurityFilter(
+//        @Value("\${eures.token}") private val ourToken: String,
+//        @Value("\${eures.uri-part}") private val uriPart: String,
+//) : OncePerRequestFilter() {
+//    private val log: Logger = LoggerFactory.getLogger(EuresSecurityFilter::class.java)
+//
+//    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+//        if(!request.requestURI.contains(uriPart))
+//            return filterChain.doFilter(request, response)
+//
+//        val authHeader: String? = request.getHeader("Authorization")
+//
+//        val token: String = authHeader?.split(' ')?.get(1)
+//                ?: "UNAUTHORIZED"
+//
+//        log.info("Our = $ourToken Header = $authHeader token = $token other headers: ${request.headerNames}")
+//
+//        if(token == ourToken)
+//            filterChain.doFilter(request, response)
+//        else
+//            response.setStatus(HttpStatus.UNAUTHORIZED.value())
+//
+//    }
+//}
 
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        if(!request.requestURI.contains(uriPart))
-            return filterChain.doFilter(request, response)
+class EuresSecurityHandler(
+        private val ourToken: String
+) : HandlerInterceptor {
+    private val log: Logger = LoggerFactory.getLogger(EuresSecurityHandler::class.java)
 
-        val authHeader: String? = request.getHeader("Authorization")
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
 
-        val token: String = authHeader?.split(' ')?.get(1)
-                ?: "UNAUTHORIZED"
+        val handlerMethod = handler as HandlerMethod
 
-        log.info("Our = $ourToken Header = $authHeader token = $token other headers: ${request.headerNames}")
+        if (handlerMethod.bean !is EuresController)
+            return true
 
-        if(token == ourToken)
-            filterChain.doFilter(request, response)
-        else
-            response.setStatus(HttpStatus.UNAUTHORIZED.value())
+        if (request.extractToken()?.equals(ourToken) == true)
+            return true
+
+        response.status = HttpStatus.UNAUTHORIZED.value()
+        return false
 
     }
+
+    fun HttpServletRequest.extractToken(): String? = getHeader("Authorization")?.split(' ')?.get(1)
+
 }
