b3d819ac3df1b48b47d259637691147d40105ac0
Einar Bjerve <einar.bjerve@nav.no>
2020-11-30 09:10:08 +0100

    Fjernet gammel kode

--- src/main/kotlin/no/nav/cv/eures/eures/EuresSecurity.kt
+++ src/main/kotlin/no/nav/cv/eures/eures/EuresSecurity.kt
@@ -13,32 +13,6 @@ import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-//@Component
-//class EuresSecurityFilter(
-//        @Value("\${eures.token}") private val ourToken: String,
-//        @Value("\${eures.uri-part}") private val uriPart: String,
-//) : OncePerRequestFilter() {
-//    private val log: Logger = LoggerFactory.getLogger(EuresSecurityFilter::class.java)
-//
-//    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-//        if(!request.requestURI.contains(uriPart))
-//            return filterChain.doFilter(request, response)
-//
-//        val authHeader: String? = request.getHeader("Authorization")
-//
-//        val token: String = authHeader?.split(' ')?.get(1)
-//                ?: "UNAUTHORIZED"
-//
-//        log.info("Our = $ourToken Header = $authHeader token = $token other headers: ${request.headerNames}")
-//
-//        if(token == ourToken)
-//            filterChain.doFilter(request, response)
-//        else
-//            response.setStatus(HttpStatus.UNAUTHORIZED.value())
-//
-//    }
-//}
-
 class EuresSecurityHandler(
         private val ourToken: String
 ) : HandlerInterceptor {
