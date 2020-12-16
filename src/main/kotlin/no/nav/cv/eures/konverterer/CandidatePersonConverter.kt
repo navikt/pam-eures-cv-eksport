df85d64029e6d2d73dfb6d37deaa0e6786a48557
Einar Bjerve <einar.bjerve@nav.no>
2020-12-16 11:22:23 +0100

    Tillate å dele CVer uten personalia

--- src/main/kotlin/no/nav/cv/eures/konverterer/CandidatePersonConverter.kt
+++ src/main/kotlin/no/nav/cv/eures/konverterer/CandidatePersonConverter.kt
@@ -17,9 +17,11 @@ class CandidatePersonConverter(
         val log = LoggerFactory.getLogger(CandidatePersonConverter::class.java)
     }
 
-    fun toXmlRepresentation() = when (samtykke.personalia) {
-        false -> throw Exception("Personalia is mandatory at this stage") // TODO : Consider how to handle when people don't want to share personalia
-        true -> CandidatePerson(
+    fun toXmlRepresentation(): CandidatePerson {
+        if (!samtykke.personalia) {
+            log.warn("Sharing CV without personalia, CvId : \"${cv.cvId}\"") // TODO : Consider how to handle when people don't want to share personalia
+        }
+        return CandidatePerson(
                 personName = Name(
                         givenName = cv.fornavn,
                         familyName = cv.etternavn),
@@ -35,13 +37,13 @@ class CandidatePersonConverter(
 
     private fun List<Spraakferdighet>.toLanguages() : List<String> {
         val languages = mapNotNull { LanguageConverter.fromIso3ToIso1(it.iso3kode) }
-        if (languages.isEmpty()) log.warn("Missing at least one language for CvId : ${cv.cvId}\"")
+        if (languages.isEmpty()) log.warn("Missing at least one language for CvId : \"${cv.cvId}\"")
         return languages
     } // TODO : Is this Aktør ID?
 
     private fun String.toIso3166_1a2CountryCode(): String {
         return NationalityConverter.getIsoCode(this)
-                ?: "".also { log.warn("Cannot find nationality code for $this CvId : ${cv.cvId}") }
+                ?: "".also { log.warn("Cannot find nationality code for $this CvId : \"${cv.cvId}") }
     } // TODO : Is this Aktør ID?
 
 }
