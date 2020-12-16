aa7e2676ab1c33ad7918fbb77f5fb35c1070aa21
Einar Bjerve <einar.bjerve@nav.no>
2020-12-16 08:43:43 +0100

    Enabling scheduling

--- src/main/kotlin/no/nav/cv/eures/Application.kt
+++ src/main/kotlin/no/nav/cv/eures/Application.kt
@@ -5,10 +5,12 @@ import org.springframework.boot.Banner
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
 @EnableJwtTokenValidation
+@EnableScheduling
 class Application
 
 fun main(args: Array<String>) {
