a0b9307d7935c431e7f31cd198dd41190d0a3f80
Einar Bjerve <einar.bjerve@nav.no>
2020-12-16 11:48:58 +0100

    Produserer også delte CVer uten personalia

--- src/main/kotlin/no/nav/cv/eures/konverterer/CandidatePersonConverter.kt
+++ src/main/kotlin/no/nav/cv/eures/konverterer/CandidatePersonConverter.kt
@@ -19,7 +19,19 @@ class CandidatePersonConverter(
 
     fun toXmlRepresentation(): CandidatePerson {
         if (!samtykke.personalia) {
-            log.warn("Sharing CV without personalia, CvId : \"${cv.cvId}\"") // TODO : Consider how to handle when people don't want to share personalia
+            log.warn("Sharing CV without personalia, CvId : \"${cv.cvId}\"")
+            return CandidatePerson(
+                    personName = Name(
+                            givenName = "",
+                            familyName = ""
+                    ),
+                    communication = listOf(),
+                    residencyCountryCode = cv.land,
+                    nationalityCode = listOf(cv.nasjonalitet.toIso3166_1a2CountryCode()),
+                    birthDate = "",
+                    genderCode = GenderCode.NotSpecified,
+                    primaryLanguageCode = cv.spraakferdigheter.toLanguages()
+            )
         }
         return CandidatePerson(
                 personName = Name(
@@ -32,18 +44,19 @@ class CandidatePersonConverter(
                 nationalityCode = listOf(cv.nasjonalitet.toIso3166_1a2CountryCode()),
                 birthDate = cv.foedselsdato.toString(),
                 genderCode = GenderCode.NotSpecified, // TODO : Vi har vel ikke kjønn?
-                primaryLanguageCode = cv.spraakferdigheter.toLanguages())
+                primaryLanguageCode = cv.spraakferdigheter.toLanguages()
+        )
     }
 
     private fun List<Spraakferdighet>.toLanguages() : List<String> {
         val languages = mapNotNull { LanguageConverter.fromIso3ToIso1(it.iso3kode) }
         if (languages.isEmpty()) log.warn("Missing at least one language for CvId : \"${cv.cvId}\"")
         return languages
-    } // TODO : Is this Aktør ID?
+    }
 
     private fun String.toIso3166_1a2CountryCode(): String {
         return NationalityConverter.getIsoCode(this)
                 ?: "".also { log.warn("Cannot find nationality code for $this CvId : \"${cv.cvId}") }
-    } // TODO : Is this Aktør ID?
+    }
 
 }
