b9ef44b9048af90494820c9a571911e14f9a690f
Einar Bjerve <einar.bjerve@nav.no>
2020-12-21 15:19:47 +0100

    Enda mer anonym logging

--- src/main/kotlin/no/nav/cv/eures/cv/CvConsumer.kt
+++ src/main/kotlin/no/nav/cv/eures/cv/CvConsumer.kt
@@ -55,7 +55,7 @@ class CvConsumer(
 
         if (foedselsnummer == null) {
             // TODO ta vekk logging av aktørid i prod
-            log.warn("Kafkamelding mangler fødselsnummer - hopper over den ($aktoerId) - Meldingstype: $meldingstype. [ endretCv: $endreCv, endretCv.cv: ${endreCv?.cv},  opprettCv: $opprettCv, opprettCv.cv: ${opprettCv?.cv} ]")
+            log.warn("Kafkamelding mangler fødselsnummer - hopper over den ($aktoerId) - Meldingstype: $meldingstype.")// [ endretCv: $endreCv, endretCv.cv: ${endreCv?.cv},  opprettCv: $opprettCv, opprettCv.cv: ${opprettCv?.cv} ]")
             return
         }
 
