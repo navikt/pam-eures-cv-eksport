5cb19c868c0000d244a7b535e618cee517df761d
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-01-12 09:39:58 +0100

    Delete from the Samtykke repo as well
    Co-authored-by: marthehagen <marthe.hagen@nav.no>
    Co-authored-by: magnusesp <magnus.s.espeland@nav.no>
    Co-authored-by: jjtg-nav <julian.torp@nav.no>

--- src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
+++ src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
@@ -1,5 +1,6 @@
 package no.nav.cv.eures.samtykke
 
+import no.nav.cv.eures.cv.CvXml
 import no.nav.cv.eures.konverterer.CvConverterService
 import org.springframework.stereotype.Service
 
@@ -11,8 +12,11 @@ class SamtykkeService(
     fun hentSamtykke(foedselsnummer: String): Samtykke? =
             samtykkeRepository.hentSamtykke(foedselsnummer)
 
-    fun slettSamtykke(foedselsnummer: String) =
-            cvConverterService.delete(foedselsnummer)
+    fun slettSamtykke(foedselsnummer: String) {
+        cvConverterService.delete(foedselsnummer)
+        samtykkeRepository.slettSamtykke(foedselsnummer)
+    }
+
 
     fun oppdaterSamtykke(foedselsnummer: String, samtykke: Samtykke) =
             samtykkeRepository.oppdaterSamtykke(foedselsnummer, samtykke)
