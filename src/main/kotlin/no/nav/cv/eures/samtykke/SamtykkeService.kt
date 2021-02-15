0e608fa7c0c26d00c85577d759f1793b11844e7b
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-02-15 12:41:55 +0100

    Only count when a new ''amtykke' is created

--- src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
+++ src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
@@ -1,22 +1,32 @@
 package no.nav.cv.eures.samtykke
 
+import io.micrometer.core.instrument.MeterRegistry
 import no.nav.cv.eures.konverterer.CvConverterService
 import org.springframework.stereotype.Service
 
 @Service
 class SamtykkeService(
         private val samtykkeRepository: SamtykkeRepository,
-        private val cvConverterService: CvConverterService
+        private val cvConverterService: CvConverterService,
+        private val meterRegistry: MeterRegistry
 ) {
     fun hentSamtykke(foedselsnummer: String): Samtykke? =
             samtykkeRepository.hentSamtykke(foedselsnummer)
 
     fun slettSamtykke(foedselsnummer: String) {
+        meterRegistry.counter("cv.eures.eksport.antall.samtykke.deleted").increment(1.0)
+
         cvConverterService.delete(foedselsnummer)
         samtykkeRepository.slettSamtykke(foedselsnummer)
     }
 
-    fun oppdaterSamtykke(foedselsnummer: String, samtykke: Samtykke) =
-            samtykkeRepository.oppdaterSamtykke(foedselsnummer, samtykke)
-                    .run { cvConverterService.createOrUpdate(foedselsnummer) }
+    fun oppdaterSamtykke(foedselsnummer: String, samtykke: Samtykke) {
+        val existing = samtykkeRepository.hentSamtykke(foedselsnummer)
+
+        if(existing == null)
+            meterRegistry.counter("cv.eures.eksport.antall.samtykke.created").increment(1.0)
+
+        samtykkeRepository.oppdaterSamtykke(foedselsnummer, samtykke)
+                .run { cvConverterService.createOrUpdate(foedselsnummer) }
+    }
 }
