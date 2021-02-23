a52a90b80fb948d4425537f1ff3c4edb60d744dd
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-02-23 15:17:31 +0100

    Revert to default meter registry
    Co-authored-by: einarbj <einar.bjerve@nav.no>
    Co-authored-by: marthehagen <marthe.hagen@nav.no>
    Co-authored-by: lljorgeluis <jorge.luis.lopez.marin@nav.no>

--- src/main/kotlin/no/nav/cv/eures/scheduled/GenerateMetrics.kt
+++ src/main/kotlin/no/nav/cv/eures/scheduled/GenerateMetrics.kt
@@ -1,7 +1,6 @@
 package no.nav.cv.eures.scheduled
 
-import io.micrometer.influx.InfluxMeterRegistry
-import io.micrometer.prometheus.PrometheusMeterRegistry
+import io.micrometer.core.instrument.MeterRegistry
 import no.nav.cv.eures.cv.CvRepository
 import no.nav.cv.eures.cv.CvXmlRepository
 import no.nav.cv.eures.eures.EuresService
@@ -16,8 +15,7 @@ import org.springframework.stereotype.Service
 @Profile("!test")
 @Service
 class GenerateMetrics(
-        private val promMeterRegistry: PrometheusMeterRegistry,
-        private val influxMeterRegistry: InfluxMeterRegistry,
+        private val meterRegistry: MeterRegistry,
         private val samtykkeRepository: SamtykkeRepository,
         private val cvRepository: CvRepository,
         private val cvXmlRepository: CvXmlRepository,
@@ -31,39 +29,30 @@ class GenerateMetrics(
     @Scheduled (fixedDelay = 1000 * 60)
     fun count() {
         val count = samtykkeRepository.hentAntallSamtykker()
-        promMeterRegistry.gauge("cv.eures.eksport.antall.samtykker.total", count)
+        meterRegistry.gauge("cv.eures.eksport.antall.samtykker.total", count)
         log.info("Metric: $count samtykker er hentet")
 
         val countRaw = cvRepository.fetchCountRawCvs()
-        promMeterRegistry.gauge("cv.eures.eksport.antall.raw.total", countRaw)
+        meterRegistry.gauge("cv.eures.eksport.antall.raw.total", countRaw)
         log.info("Metric:$countRaw RawCV-er er hentet")
 
         val countExportable = cvXmlRepository.fetchAllActive().size
-        promMeterRegistry.gauge("cv.eures.eksport.antall.exportable.total", countExportable)
+        meterRegistry.gauge("cv.eures.eksport.antall.exportable.total", countExportable)
         log.info("Metric:$countExportable eksporterbare CV-er er hentet")
 
         val countDeletable = cvXmlRepository.fetchCountDeletableCvs()
-        promMeterRegistry.gauge("cv.eures.eksport.antall.deletable.total", countDeletable)
+        meterRegistry.gauge("cv.eures.eksport.antall.deletable.total", countDeletable)
         log.info("Metric:$countDeletable slettbare CV-er er hentet")
 
         val (created, modified, closed) = euresService.getAll()
-        promMeterRegistry.gauge("cv.eures.eksport.antall.euresService.created.total", created.size)
-        promMeterRegistry.gauge("cv.eures.eksport.antall.euresService.modified.total", modified.size)
-        promMeterRegistry.gauge("cv.eures.eksport.antall.euresService.closed.total", closed.size)
+        meterRegistry.gauge("cv.eures.eksport.antall.euresService.created.total", created.size)
+        meterRegistry.gauge("cv.eures.eksport.antall.euresService.modified.total", modified.size)
+        meterRegistry.gauge("cv.eures.eksport.antall.euresService.closed.total", closed.size)
         log.info("Metric: ${created.size} opprettet, ${modified.size} endret, ${closed.size} slettet")
 
         val countEscoCache = janzzCacheRepository.getCacheCount()
-        promMeterRegistry.gauge("cv.eures.eksport.antall.escoCache.total", countEscoCache)
+        meterRegistry.gauge("cv.eures.eksport.antall.escoCache.total", countEscoCache)
         log.info("Metric: $countEscoCache linjer i ESCO cache")
-
-        val influxCreated = influxMeterRegistry.counter("cv.eures.eksport.samtykke.opprettet").count()
-        promMeterRegistry.gauge("cv.eures.eksport.influx.samtykke.opprettet", influxCreated)
-        log.info("Metric: $influxCreated Influx opprettet")
-
-        val influxDeleted = influxMeterRegistry.counter("cv.eures.eksport.samtykke.slettet").count()
-        promMeterRegistry.gauge("cv.eures.eksport.influx.samtykke.slettet", influxDeleted)
-        log.info("Metric: $influxDeleted Influx slettet")
-
 
     }
 }
