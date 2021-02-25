ee9d2e86be6b218ac08f8407708b3b77742543b9
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-02-25 12:09:25 +0100

    Failing influx ripples upwards Co-authored-by: marthehagen <marthe.hagen@nav.no>

--- src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
+++ src/main/kotlin/no/nav/cv/eures/samtykke/SamtykkeService.kt
@@ -2,6 +2,8 @@ package no.nav.cv.eures.samtykke
 
 import io.micrometer.core.instrument.MeterRegistry
 import no.nav.cv.eures.konverterer.CvConverterService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,11 +12,19 @@ class SamtykkeService(
         private val cvConverterService: CvConverterService,
         private val meterRegistry: MeterRegistry
 ) {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(SamtykkeService::class.java)
+    }
+
     fun hentSamtykke(foedselsnummer: String): Samtykke? =
             samtykkeRepository.hentSamtykke(foedselsnummer)
 
     fun slettSamtykke(foedselsnummer: String) {
-        meterRegistry.counter("cv.eures.eksport.samtykke.slettet").increment(1.0)
+        try {
+            meterRegistry.counter("cv.eures.eksport.samtykke.slettet").increment(1.0)
+        } catch (e: Exception) {
+            log.warn("Got exception when trying to increase metric counter for deleted samtykke", e)
+        }
 
         cvConverterService.delete(foedselsnummer)
         samtykkeRepository.slettSamtykke(foedselsnummer)
@@ -23,8 +33,12 @@ class SamtykkeService(
     fun oppdaterSamtykke(foedselsnummer: String, samtykke: Samtykke) {
         val existing = samtykkeRepository.hentSamtykke(foedselsnummer)
 
-        if(existing == null)
-            meterRegistry.counter("cv.eures.eksport.samtykke.opprettet").increment(1.0)
+        try {
+            if(existing == null)
+                meterRegistry.counter("cv.eures.eksport.samtykke.opprettet").increment(1.0)
+        } catch (e: Exception) {
+            log.warn("Got exception when trying to increase metric counter for created samtykke", e)
+        }
 
         samtykkeRepository.oppdaterSamtykke(foedselsnummer, samtykke)
                 .run { cvConverterService.createOrUpdate(foedselsnummer) }
