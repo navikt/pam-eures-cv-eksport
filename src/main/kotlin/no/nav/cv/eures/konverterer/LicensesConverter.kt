6c9c0a0d5c2a43a378495808cdf37beee90dbfeb
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-03-04 09:55:55 +0100

    Debug licences

--- src/main/kotlin/no/nav/cv/eures/konverterer/LicensesConverter.kt
+++ src/main/kotlin/no/nav/cv/eures/konverterer/LicensesConverter.kt
@@ -6,12 +6,18 @@ import no.nav.cv.eures.model.FreeFormPeriod
 import no.nav.cv.eures.model.License
 import no.nav.cv.eures.model.Licenses
 import no.nav.cv.eures.samtykke.Samtykke
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class LicensesConverter (
         private val cv: Cv,
         private val samtykke: Samtykke
 ) {
     private val ikkeSamtykket = null
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(CertificationConverter::class.java)
+    }
 
     fun toXmlRepresentation()
             = when(samtykke.foererkort) {
@@ -21,6 +27,8 @@ class LicensesConverter (
 
     fun Foererkort.toLicenses()
             = Licenses(klasse.map {
+                if(cv.aktoerId == "2308808164824")
+                    log.debug("FOERERKORT ${cv.aktoerId}: Mapping klasse ${it.klasse} and beskrivelse ${it.klasseBeskrivelse}")
                 License(
                         licenseTypeCode = it.klasse,
                         licenseName = it.klasseBeskrivelse,
