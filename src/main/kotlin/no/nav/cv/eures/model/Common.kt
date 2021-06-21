b6bf63b8dd4d05318260357666ca10383618a221
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-06-21 14:40:53 +0200

    Fix NPE. Some old education attendance entries doesn't have a start date.

--- src/main/kotlin/no/nav/cv/eures/model/Common.kt
+++ src/main/kotlin/no/nav/cv/eures/model/Common.kt
@@ -2,7 +2,7 @@ package no.nav.cv.eures.model
 
 // 4.13.7.2
 data class AttendancePeriod(
-        val startDate: FormattedDateTime,
+        val startDate: FormattedDateTime?,
         val endDate: FormattedDateTime?
 )
 
