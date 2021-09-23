eefb2212c9cf954ead894db05badb87c2e65bb51
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-09-23 13:35:52 +0200

    Add an interface to trigger reprocessing of CVs

--- src/main/kotlin/no/nav/cv/eures/scheduled/XmlUpdater.kt
+++ src/main/kotlin/no/nav/cv/eures/scheduled/XmlUpdater.kt
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
-@Profile("!test")
 @Service
 class XmlUpdater (
         private val cvConverterService: CvConverterService,
