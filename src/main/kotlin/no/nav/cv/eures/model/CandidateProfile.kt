9d47829e31418c7b5633068ac7ff69f2228a332f
bin <yongbinkwon97@gmail.com>
2022-02-18 14:04:33 +0100

    refactor CandidateProfile

--- src/main/kotlin/no/nav/cv/eures/model/CandidateProfile.kt
+++ src/main/kotlin/no/nav/cv/eures/model/CandidateProfile.kt
@@ -2,6 +2,7 @@ package no.nav.cv.eures.model
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import no.nav.arbeid.cv.avro.Cv
+import no.nav.arbeid.cv.avro.FagdokumentasjonType
 import no.nav.arbeid.cv.avro.Jobbprofil
 import no.nav.cv.eures.janzz.dto.CachedEscoMapping
 import no.nav.cv.eures.model.dto.ArbeidserfaringWithEscoDto
@@ -9,30 +10,30 @@ import no.nav.cv.eures.samtykke.Samtykke
 
 // 4.11
 data class CandidateProfile(
-        @JacksonXmlProperty(isAttribute = true, localName = "languageCode")
-        val languageCode: String = "no",
-        val executiveSummary: String = "",
-        val employmentHistory: EmploymentHistory? = null,
-        val educationHistory: EducationHistory? = null,
-        val licenses: Licenses? = null,
-        val certifications: Certifications? = null,
-        val personQualifications: PersonQualifications? = null
+    @JacksonXmlProperty(isAttribute = true, localName = "languageCode")
+    val languageCode: String = "no",
+    val executiveSummary: String = "",
+    val employmentHistory: EmploymentHistory? = null,
+    val educationHistory: EducationHistory? = null,
+    val licenses: Licenses? = null,
+    val certifications: Certifications? = null,
+    val personQualifications: PersonQualifications? = null
 ) {
     constructor(
         samtykke: Samtykke,
         cv: Cv,
         jobbProfil: Jobbprofil,
         arbeidserfaringerWithEsco: List<ArbeidserfaringWithEscoDto>
-    ): this(
-        executiveSummary = if(samtykke.sammendrag) cv.sammendrag else "",
-        employmentHistory = if(samtykke.arbeidserfaring) {
+    ) : this(
+        executiveSummary = if (samtykke.sammendrag) cv.sammendrag else "",
+        employmentHistory = if (samtykke.arbeidserfaring) {
             EmploymentHistory(
                 employerHistory = arbeidserfaringerWithEsco.map {
                     EmployerHistory(it)
                 }
             )
         } else null,
-        educationHistory = if(samtykke.utdanning) {
+        educationHistory = if (samtykke.utdanning) {
             EducationHistory(
                 educationOrganizationAttendance = cv.utdannelse.map {
                     EducationOrganizationAttendance(
@@ -41,7 +42,7 @@ data class CandidateProfile(
                 }
             )
         } else null,
-        licenses = if(samtykke.foererkort) {
+        licenses = if (samtykke.foererkort) {
             Licenses(
                 license = cv.foererkort.klasse.map {
                     License(
@@ -51,14 +52,56 @@ data class CandidateProfile(
             )
         } else null,
         certifications = Certifications(
-            listOf<Certification>().apply {  }
-            if(samtykke.offentligeGodkjenninger) {
+            if (samtykke.offentligeGodkjenninger) {
                 cv.godkjenninger.mapNotNull {
+                    it.tittel ?: return@mapNotNull null
+                    it.utsteder ?: return@mapNotNull null
                     Certification(it)
                 }
             } else {
                 listOf()
-            }
+            }.plus(
+                if (samtykke.andreGodkjenninger) {
+                    cv.sertifikat.mapNotNull {
+                        it.tittel ?: return@mapNotNull null
+                        it.utsteder ?: return@mapNotNull null
+                        Certification(it)
+                    }
+                } else {
+                    listOf()
+                }
+            ).plus(
+                if (samtykke.kurs) {
+                    cv.kurs.mapNotNull {
+                        it.tittel ?: return@mapNotNull null
+                        it.utsteder ?: return@mapNotNull null
+                        Certification(it)
+                    }
+                } else {
+                    listOf()
+                }
+            ).plus(
+                if (samtykke.offentligeGodkjenninger && samtykke.fagbrev) {
+                    cv.fagdokumentasjon.mapNotNull {
+                        it.tittel ?: return@mapNotNull null
+                        Certification(it)
+                    }
+                } else if (samtykke.offentligeGodkjenninger) {
+                    cv.fagdokumentasjon.mapNotNull {
+                        it.tittel ?: return@mapNotNull null
+                        if (it.type != FagdokumentasjonType.AUTORISASJON) return@mapNotNull null
+                        Certification(it)
+                    }
+                } else if (samtykke.fagbrev) {
+                    cv.fagdokumentasjon.mapNotNull {
+                        it.tittel ?: return@mapNotNull null
+                        if (it.type == FagdokumentasjonType.AUTORISASJON) return@mapNotNull null
+                        Certification(it)
+                    }
+                } else {
+                    listOf()
+                }
+            )
         ).nullIfNoCerts()
     )
 }
