b5d8a3309c247828af20cf23068311ceb71dac63
haugse <sara.haugse@gmail.com>
2022-09-19 14:15:26 +0200

    Fjernet noen ubrukte imports

--- src/main/kotlin/no/nav/cv/eures/Application.kt
+++ src/main/kotlin/no/nav/cv/eures/Application.kt
@@ -2,7 +2,6 @@ package no.nav.cv.eures
 
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.Banner
-import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
