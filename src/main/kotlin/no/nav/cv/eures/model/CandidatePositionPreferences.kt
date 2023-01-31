28ff016b852795c6f44bc24c2e3ce8863bf43c1b
noraness <nora.storebo.ness@nav.no>
2023-01-31 12:08:28 +0100

    Fjern nullable for statisk verdi.

--- src/main/kotlin/no/nav/cv/eures/model/CandidatePositionPreferences.kt
+++ src/main/kotlin/no/nav/cv/eures/model/CandidatePositionPreferences.kt
@@ -12,10 +12,10 @@ data class PreferredLocation(
 
 data class ReferenceLocation(
     @JacksonXmlProperty(isAttribute = true, localName = "listName")
-    val listName: String?= "Countries",
+    val listName: String = "Countries",
     @JacksonXmlProperty(isAttribute = true, localName = "listURI")
-    val listURI: String?= "http://ec.europa.eu/esco/ConceptScheme/country",
+    val listURI: String = "http://ec.europa.eu/esco/ConceptScheme/country",
     @JacksonXmlProperty(isAttribute = true, localName = "listName")
-    val listVersionID: String?= "3166-1-alpha-2",
+    val listVersionID: String = "3166-1-alpha-2",
     val countryCode: String
 )
