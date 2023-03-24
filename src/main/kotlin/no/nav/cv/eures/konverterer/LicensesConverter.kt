6ecfdffdc6e6f56c1c7cb6d1063829efbfce5b2d
noraness <110546349+noraness@users.noreply.github.com>
2023-03-24 12:06:08 +0100

    Kun spesifikke licenses godkjennes av eures. Valideringsfeil på S, T og AM147 feks (#62)
    * Sender kun godkjente rettigheter til Eures.

--- src/main/kotlin/no/nav/cv/eures/konverterer/LicensesConverter.kt
+++ src/main/kotlin/no/nav/cv/eures/konverterer/LicensesConverter.kt
@@ -8,28 +8,38 @@ import no.nav.cv.eures.model.Licenses
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class LicensesConverter (
-        private val dto: CvEndretInternDto
+class LicensesConverter(
+    private val dto: CvEndretInternDto
 ) {
     private val ikkeSamtykket = null
 
     companion object {
         val log: Logger = LoggerFactory.getLogger(LicensesConverter::class.java)
+        val euresAcceptedLicenseTypeCodes =
+            listOf("AM", "A1", "A2", "A", "B1", "B", "BE", "C1", "C1E", "C", "CE", "D1", "D1E", "DE", "D");
     }
 
-    fun toXmlRepresentation() : Licenses? {
+    fun toXmlRepresentation(): Licenses? {
         return dto.cv?.driversLicenses?.toLicenses()
     }
 
-    fun List<CvEndretInternDriversLicence>.toLicenses() : Licenses{
-        return Licenses(map { cvEndretInternDriversLicence ->  License(
-            licenseTypeCode = cvEndretInternDriversLicence.klasse ?: "",
-            licenseName = cvEndretInternDriversLicence.description ?: "",
-            freeFormPeriod = FreeFormPeriod(
-                startDate = cvEndretInternDriversLicence.acquiredDate?.toFormattedDateTime(),
-                endDate = cvEndretInternDriversLicence.expiryDate?.toFormattedDateTime()
+    fun List<CvEndretInternDriversLicence>.toLicenses(): Licenses {
+        return Licenses(filter { cvEndretInternDriversLicence1 ->
+            euresAcceptedLicenseTypeCodes.contains(
+                cvEndretInternDriversLicence1.klasse
             )
-        )})
+        }
+            .map { cvEndretInternDriversLicence ->
+                License(
+                    licenseTypeCode = cvEndretInternDriversLicence.klasse ?: "",
+                    licenseName = cvEndretInternDriversLicence.description ?: "",
+                    freeFormPeriod = FreeFormPeriod(
+                        startDate = cvEndretInternDriversLicence.acquiredDate?.toFormattedDateTime(),
+                        endDate = cvEndretInternDriversLicence.expiryDate?.toFormattedDateTime()
+                    )
+                )
+            }
+        )
     }
-
 }
+
