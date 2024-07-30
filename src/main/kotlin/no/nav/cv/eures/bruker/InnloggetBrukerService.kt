1f82d0e7d465f195626505a2f68802612c879c5d
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-07-30 13:28:37 +0200

    Tester å logge for å identifsere feilkilde

--- src/main/kotlin/no/nav/cv/eures/bruker/InnloggetBrukerService.kt
+++ src/main/kotlin/no/nav/cv/eures/bruker/InnloggetBrukerService.kt
@@ -2,12 +2,16 @@ package no.nav.cv.eures.bruker
 
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.core.jwt.JwtTokenClaims
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class InnloggetBrukerService (
     private val contextHolder: TokenValidationContextHolder
 ) : InnloggetBruker {
+    companion object {
+        val logger = LoggerFactory.getLogger(InnloggetBrukerService::class.java)
+    }
 
     override fun fodselsnummer(): String {
         val context = contextHolder.tokenValidationContext
@@ -15,7 +19,12 @@ class InnloggetBrukerService (
         var claims : JwtTokenClaims? = null
 
         if(issuers.isNotEmpty()) {
-            claims = context.getClaims(issuers[0])
+            try {
+                claims = context.getClaims(issuers[0])
+            } catch (e: Exception) {
+                logger.warn("Kunne ikke hente claims fra token i InnloggetBrukerService", e)
+                throw e
+            }
         }
 
         if(claims == null) {
