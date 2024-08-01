d364fe640db6a445227579b60d34eb4574119ce9
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2024-08-01 13:14:04 +0200

    Lagrer ny jsonCv og underOppfoelging når bruker ikke lenger er under oppfølging (#72)

--- src/main/kotlin/no/nav/cv/eures/cv/CvRawService.kt
+++ src/main/kotlin/no/nav/cv/eures/cv/CvRawService.kt
@@ -5,78 +5,68 @@ import no.nav.cv.eures.samtykke.SamtykkeService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.ZonedDateTime
 
 @Service
-class CvRawService(
-    private val cvRepository: CvRepository,
-    private val samtykkeService: SamtykkeService
-    ) {
-
+class CvRawService(private val cvRepository: CvRepository, private val samtykkeService: SamtykkeService) {
     companion object {
         val log: Logger = LoggerFactory.getLogger(CvRawService::class.java)
     }
 
-    fun createOrUpdateRawCvRecord(dto: CvEndretInternDto, cvAsJson: String) {
-        val foedselsnummer = dto.fodselsnummer
-        val aktoerId = dto.aktorId
+    @Transactional
+    fun updateRawCvRecord(eksisterende: RawCV, dto: CvEndretInternDto, cvAsJson: String) {
+        val erUnderOppfølging = dto.oppfolgingsInformasjon?.erUnderOppfolging ?: false
+        val eksisterendeErUnderOppfølging = eksisterende.underOppfoelging
 
-        if (foedselsnummer == null) {
-            log.warn("Kafkamelding mangler fødselsnummer - hopper over den (${aktoerId}) - Meldingstype: ${dto.meldingstype}.")
+        eksisterende.update(
+            sistEndret = ZonedDateTime.now(),
+            jsonCv = cvAsJson,
+            underOppfoelging = erUnderOppfølging,
+            meldingstype = RawCV.Companion.RecordType.UPDATE
+        )
+
+        try {
+            if (eksisterendeErUnderOppfølging && !erUnderOppfølging) samtykkeService.slettSamtykke(eksisterende.foedselsnummer)
+            cvRepository.saveAndFlush(eksisterende)
+        } catch (e: Exception) {
+            log.error("Fikk exception ${e.message} under oppdatring av cv $this", e)
+        }
+    }
+
+    @Transactional
+    fun createRawCvRecord(dto: CvEndretInternDto, cvAsJson: String) {
+        cvRepository.deleteCvByAktorId(dto.aktorId)
+
+        val newRawCv = RawCV.create(
+            aktoerId = dto.aktorId,
+            foedselsnummer = dto.fodselsnummer!!,
+            sistEndret = ZonedDateTime.now(),
+            jsonCv = cvAsJson,
+            underOppfoelging = dto.oppfolgingsInformasjon?.erUnderOppfolging,
+            meldingstype = RawCV.Companion.RecordType.CREATE
+        )
+
+        try {
+            cvRepository.saveAndFlush(newRawCv)
+        } catch (e: Exception) {
+            log.error("Fikk exception ${e.message} under lagring av cv $this", e)
+        }
+    }
+
+    @Transactional
+    fun createOrUpdateRawCvRecord(dto: CvEndretInternDto, cvAsJson: String) {
+        if (dto.fodselsnummer == null) {
+            log.warn("Kafkamelding mangler fødselsnummer - hopper over den (${dto.aktorId}) - Meldingstype: ${dto.meldingstype}.")
             return
         }
 
-        val existing = cvRepository.hentCvByFoedselsnummer(foedselsnummer)
-
-        if(existing != null) {
-            if (existing.underOppfoelging && (dto.oppfolgingsInformasjon != null && !dto.oppfolgingsInformasjon.erUnderOppfolging)) {
-                log.debug("Deleting ${existing.aktoerId} due to not being 'under oppfølging' anymore")
-
-                // By deleting samtykke we also mark the XML CV for deletion
-                try {
-                    samtykkeService.slettSamtykke(foedselsnummer)
-                } catch (e: Exception) {
-                    log.error("Fikk exception ${e.message} under sletting av cv $this", e)
-                }
-            } else {
-                log.debug("Updating ${existing.aktoerId}")
-                existing.update(
-                    sistEndret = ZonedDateTime.now(),
-                    jsonCv = cvAsJson,
-                    underOppfoelging = (dto.oppfolgingsInformasjon != null),
-                    meldingstype = RawCV.Companion.RecordType.UPDATE
-                )
-
-                try {
-                    cvRepository.saveAndFlush(existing)
-                } catch (e: Exception) {
-                    log.error("Fikk exception ${e.message} under oppdatring av cv $this", e)
-                }
-            }
-        } else {
-            log.debug("inserting new record for $aktoerId")
-
-            cvRepository.deleteCvByAktorId(aktoerId)
-
-            val newRawCv = RawCV.create(
-                aktoerId = aktoerId,
-                foedselsnummer = foedselsnummer,
-                sistEndret = ZonedDateTime.now(),
-                jsonCv = cvAsJson,
-                underOppfoelging = dto.oppfolgingsInformasjon?.erUnderOppfolging,
-                meldingstype = RawCV.Companion.RecordType.CREATE
-            )
-
-            try {
-                cvRepository.saveAndFlush(newRawCv)
-            } catch (e: Exception) {
-                log.error("Fikk exception ${e.message} under lagring av cv $this", e)
-            }
-        }
+        cvRepository.hentCvByFoedselsnummer(dto.fodselsnummer)
+            ?.let { updateRawCvRecord(it, dto, cvAsJson) }
+            ?: createRawCvRecord(dto, cvAsJson)
     }
-    fun deleteCv(aktoerId: String): RawCV? = cvRepository.hentCvByAktoerId(aktoerId)?.update(
-        sistEndret = ZonedDateTime.now(),
-        underOppfoelging = false,
-        meldingstype = RawCV.Companion.RecordType.DELETE
-    )?.let { cvRepository.saveAndFlush(it) }
+
+    fun deleteCv(aktoerId: String): RawCV? = cvRepository.hentCvByAktoerId(aktoerId)
+        ?.update(sistEndret = ZonedDateTime.now(), underOppfoelging = false, meldingstype = RawCV.Companion.RecordType.DELETE)
+        ?.let { cvRepository.saveAndFlush(it) }
 }
